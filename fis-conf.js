f059a8170bc6afd0e92dfb4e9458d4e5f7240dd7
Fancy <fancyboynet@gmail.com>
2017-06-05 18:32:22 +0800

    简化结构

--- fis-conf.js
+++ fis-conf.js
@@ -1,10 +1,3 @@
 "use strict"
-let fis = require('fis3')
-let configBuild = require('./config/build.json')
-
-fis.set('namespace', configBuild.namespace)  //命名空间
-fis.set('staticRoot', configBuild.namespace) //静态资源目录
-fis.set('tplRoot', configBuild.namespace) //模板目录
-fis.set('staticUrlPrefix', configBuild.namespace) //静态资源前缀
-fis.set('outputRoot', configBuild.namespace) //输出目录
+const fis = require('fis3')
 
