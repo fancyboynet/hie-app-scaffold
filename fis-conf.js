076f54ab51c4b5fb7b202fdd949071be0893d34d
Fancy <fancyboynet@gmail.com>
2017-06-06 09:59:55 +0800

    fis-conf中直接可以访问fis对象

--- fis-conf.js
+++ fis-conf.js
@@ -1,3 +1,6 @@
 "use strict"
-const fis = require('fis3')
+// fis.set('outputRoot', '../') //输出目录
+// fis.set('staticRoot', 'static')//静态资源目录
+// fis.set('tplRoot', 'templates')//模板目录
+// fis.set('staticUrlPrefix', '/')//静态资源前缀
 
